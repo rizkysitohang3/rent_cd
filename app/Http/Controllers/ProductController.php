31cc008a7c4688d75a1f9386cfc81c9f36071206
rizkysitohang3 <rizkysitohang3@gmail.com>
2020-03-17 13:34:51 +0700

    Add Controller Method for rent product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
+use App\RentTransaction;
+use App\User;
+use Carbon\Carbon;
 
 class ProductController extends Controller
 {
@@ -34,12 +37,44 @@ class ProductController extends Controller
     public function updateProductStock(Request $request,$id){
 			$newvalue = $request->input('quantity');
 			$product = Product::where('id',$id)->update(['quantity' => $newvalue]);
-			return ['status'=>'success','message'=>'update success'];
+			$response_data = ['status'=>'success','message'=>'update success'];
+			return response()->json($response_data);
 		
 			
 		}
     
-    
+    public function rentProduct(Request $request, $id){
+		$user_id = $request->input('user_id');
+		$user = User::find($user_id);
+		$product = Product::find($id);
+		if(!$product ){
+			$response_data = ['status'=>'failed','message'=>'product not found'];
+			return response()->json($response_data);
+			}
+		
+		if(!$user ){
+			$response_data = ['status'=>'failed','message'=>'user not found'];
+			return response()->json($response_data);
+			}
+		
+		
+		
+		if ($product->quantity > 0) {
+				
+				$product->quantity = ($product->quantity) - 1;
+				$product->save();
+				RentTransaction::create(['user_id'=> $user_id,'product_id'=>$product->id , 'rent_date' => Carbon::today(),'returned'=> false]);
+				$response_data = ['status'=>'success','message'=>'rent success'];
+				return response()->json($response_data);
+			}else{
+				
+				$response_data = ['status'=>'failed','message'=>'product out of stock'];
+				return response()->json($response_data);
+				}
+			
+		
+		
+		}
     
     
 }
